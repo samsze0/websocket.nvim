9912d81615e6820ef5755719fc4a86a268f2f7ca
Sam Sze <mingsum.sam@gmail.com>
2024-12-10 17:44:52 +0800

    improve client error handling

--- rust/src/client/mod.rs
+++ rust/src/client/mod.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::sync::Arc;
 
-use futures_util::{SinkExt, StreamExt};
+use futures_util::{SinkExt, StreamExt, TryFutureExt};
 use mlua::prelude::{LuaFunction, LuaTable};
 use nvim_oxi::libuv::AsyncHandle;
 use nvim_oxi::{mlua::lua, schedule};
@@ -37,16 +37,20 @@ struct WebsocketClient {
 }
 
 async fn start_client(
-    connect_addr: Url,
-    extra_headers: HashMap<String, String>,
-    inbound_event_publisher: UnboundedSender<WebsocketClientInboundEvent>,
-    lua_handle: AsyncHandle,
+    connect_addr: &Url,
+    extra_headers: &HashMap<String, String>,
+    inbound_event_publisher: &UnboundedSender<WebsocketClientInboundEvent>,
+    lua_handle: &AsyncHandle,
     mut close_connection_event_subscriber: UnboundedReceiver<WebsocketClientCloseConnectionEvent>,
     mut outbound_message_receiver: UnboundedReceiver<String>,
-) {
+) -> Result<(), WebsocketClientError> {
     let send_event = move |event: WebsocketClientInboundEvent| {
-        inbound_event_publisher.send(event).unwrap();
-        lua_handle.send().unwrap();
+        inbound_event_publisher
+            .send(event)
+            .unwrap_or_else(|err| error!("Failed to send event: {}", err));
+        lua_handle
+            .send()
+            .unwrap_or_else(|err| error!("Failed to send event: {}", err));
     };
 
     let default_port = match connect_addr.scheme() {
@@ -59,8 +63,15 @@ async fn start_client(
             "Host",
             format!(
                 "{}:{}",
-                connect_addr.host_str().unwrap(),
-                connect_addr.port().unwrap_or(default_port)
+                connect_addr
+                    .host_str()
+                    .ok_or(WebsocketClientError::ConnectionError(
+                        "Host is not set".to_string()
+                    ))?,
+                connect_addr.port().unwrap_or_else(|| {
+                    info!("Port is not set. Using default port: {}", default_port);
+                    default_port
+                })
             ),
         )
         .header(
@@ -72,15 +83,9 @@ async fn start_client(
         .header("Sec-WebSocket-Version", 13)
         .uri(connect_addr.as_str())
         .body(())
-        .map_err(|err| {
-            send_event(WebsocketClientInboundEvent::Error(
-                WebsocketClientError::ConnectionError(err.to_string()),
-            ));
-            err
-        })
-        .unwrap();
+        .map_err(|err| WebsocketClientError::ConnectionError(err.to_string()))?;
 
-    for (key, value) in extra_headers {
+    for (key, value) in extra_headers.clone() {
         debug!("Adding header: {}={}", key, value);
 
         // https://stackoverflow.com/questions/23975391/how-to-convert-a-string-into-a-static-str
@@ -94,13 +99,7 @@ async fn start_client(
 
     let (ws_stream, _response) = tokio_tungstenite::connect_async(request)
         .await
-        .map_err(|err| {
-            send_event(WebsocketClientInboundEvent::Error(
-                WebsocketClientError::ConnectionError(err.to_string()),
-            ));
-            err
-        })
-        .unwrap();
+        .map_err(|err| WebsocketClientError::ConnectionError(err.to_string()))?;
     info!("{} WebSocket handshake completed", connect_addr.as_str());
     send_event(WebsocketClientInboundEvent::Connected);
 
@@ -111,17 +110,24 @@ async fn start_client(
             message = ws_receiver.next() => {
                 match message {
                     Some(message) => {
-                        let message = message.unwrap();
-                        if message.is_text() {
-                            let data = message.into_text().expect("Message received from server is not valid string");
-                            info!("Received message: {}", data);
-                            send_event(WebsocketClientInboundEvent::NewMessage(data));
-                        } else if message.is_binary() {
-                            send_event(WebsocketClientInboundEvent::Error(WebsocketClientError::ReceiveMessageError("Binary data is not supported".to_string())));
-                            error!("Binary data is not supported");
-                        } else if message.is_close() {
-                            info!("Received close frame from server");
-                            break;
+                        match message {
+                            Ok(message) => {
+                                if message.is_text() {
+                                    let data = message.into_text().expect("Message received from server is not valid string");
+                                    info!("Received message: {}", data);
+                                    send_event(WebsocketClientInboundEvent::NewMessage(data));
+                                } else if message.is_binary() {
+                                    send_event(WebsocketClientInboundEvent::Error(WebsocketClientError::ReceiveMessageError("Binary data is not supported".to_string())));
+                                    error!("Binary data is not supported");
+                                } else if message.is_close() {
+                                    info!("Received close frame from server");
+                                    break;
+                                }
+                            }
+                            Err(err) => {
+                                send_event(WebsocketClientInboundEvent::Error(WebsocketClientError::ReceiveMessageError(err.to_string())));
+                                error!("Failed to receive message: {}", err);
+                            }
                         }
                     }
                     None => (),
@@ -132,7 +138,10 @@ async fn start_client(
                     Some(close_event) => {
                         match close_event {
                             WebsocketClientCloseConnectionEvent::Graceful => {
-                                ws_sender.send(tungstenite::Message::Close(None)).await.unwrap();
+                                if let Err(err) = ws_sender.send(tungstenite::Message::Close(None)).await {
+                                    send_event(WebsocketClientInboundEvent::Error(WebsocketClientError::ConnectionError(err.to_string())));
+                                    error!("Failed to send close message: {}", err);
+                                }
                             }
                             WebsocketClientCloseConnectionEvent::Forceful => {
                                 break
@@ -145,7 +154,10 @@ async fn start_client(
             message = outbound_message_receiver.recv() => {
                 match message {
                     Some(message) => {
-                        ws_sender.send(tungstenite::Message::Text(message)).await.unwrap();
+                        if let Err(err) = ws_sender.send(tungstenite::Message::Text(message)).await {
+                            send_event(WebsocketClientInboundEvent::Error(WebsocketClientError::ConnectionError(err.to_string())));
+                            error!("Failed to forward message to websocket: {}", err);
+                        }
                     }
                     None => (),
                 }
@@ -155,6 +167,7 @@ async fn start_client(
 
     info!("Closing WebSocket connection. Sending out event - \"Disconnected\"");
     send_event(WebsocketClientInboundEvent::Disconnected);
+    Ok(())
 }
 
 impl WebsocketClient {
@@ -169,7 +182,7 @@ impl WebsocketClient {
 
         let callbacks = WebsocketClientCallbacks::new(id)?;
 
-        let (inbound_event_publisher, mut inbound_event_receiver) =
+        let (mut inbound_event_publisher, mut inbound_event_receiver) =
             mpsc::unbounded_channel::<WebsocketClientInboundEvent>();
 
         let (outbound_message_publisher, outbound_message_receiver) =
@@ -178,7 +191,7 @@ impl WebsocketClient {
         let (close_connection_event_publisher, close_connection_event_subscriber) =
             mpsc::unbounded_channel::<WebsocketClientCloseConnectionEvent>();
 
-        let lua_handle = AsyncHandle::new(move || {
+        let mut lua_handle = AsyncHandle::new(move || {
             let event = inbound_event_receiver.blocking_recv().unwrap();
             info!("Received event - \"{:?}\"", event);
             let callbacks = callbacks.clone();
@@ -215,16 +228,25 @@ impl WebsocketClient {
         let inbound_event_publisher_clone = inbound_event_publisher.clone();
         let lua_handle_clone = lua_handle.clone();
 
+        // REFACTOR: convert type to a MapErr<JoinHandle<Result<(), WebsocketClientError>>>? So that we don't need to duplicate the error handling logic
         let handle: JoinHandle<()> = ASYNC_RUNTIME.spawn(async move {
-            start_client(
-                connect_addr_clone,
-                extra_headers_clone,
-                inbound_event_publisher_clone,
-                lua_handle_clone,
+            if let Err(err) = start_client(
+                &connect_addr_clone,
+                &extra_headers_clone,
+                &inbound_event_publisher_clone,
+                &lua_handle_clone,
                 close_connection_event_subscriber,
                 outbound_message_receiver,
             )
-            .await;
+            .await
+            {
+                inbound_event_publisher_clone
+                    .send(WebsocketClientInboundEvent::Error(err))
+                    .unwrap_or_else(|err| error!("Failed to send event: {}", err));
+                lua_handle_clone
+                    .send()
+                    .unwrap_or_else(|err| error!("Failed to send event: {}", err));
+            };
 
             println!("WebSocket client has finished.");
             WEBSOCKET_CLIENT_REGISTRY.lock().remove(&id);
@@ -244,8 +266,12 @@ impl WebsocketClient {
     }
 
     fn send_event(&self, event: WebsocketClientInboundEvent) {
-        self.inbound_event_publisher.send(event).unwrap();
-        self.lua_handle.send().unwrap();
+        self.inbound_event_publisher
+            .send(event)
+            .unwrap_or_else(|err| error!("Failed to send event: {}", err));
+        self.lua_handle
+            .send()
+            .unwrap_or_else(|err| error!("Failed to send event: {}", err));
     }
 
     fn disconnect(&mut self) {
